3268dfd98d3e737f179f76e9e002852ac6d33713
sudorishabh <rishabhnegi175@gmail.com>
2026-06-04 13:03:50 +0530

    feat(blog): remove the floating dock navbar (component preserved)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { DATA } from "../data/resume";
@@ -75,10 +74,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute='class'
           defaultTheme='light'>
-          <TooltipProvider delayDuration={0}>
-            {children}
-            <Navbar />
-          </TooltipProvider>
+          <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
         </ThemeProvider>
       </body>
     </html>
